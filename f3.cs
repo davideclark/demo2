17f1d74641dd4cca90c445b2985f02db7108d54a
David Clark <dave.clark@ntlworld.com>
2015-05-31 15:22:24 +0100

    Added some test content
    Demo

---
+++ f3.cs
@@ -0,0 +1,14 @@
+ Volume in drive C is Windows8_OS
+ Volume Serial Number is B6DA-FC95
+
+ Directory of c:\git\demo2
+
+31/05/2015  15:20    <DIR>          .
+31/05/2015  15:20    <DIR>          ..
+19/12/2014  05:57               395 .gitattributes
+31/05/2015  15:17             3,511 .gitignore
+31/05/2015  15:17               425 f1.txt
+31/05/2015  15:18               511 f2.txt
+31/05/2015  15:20                 0 f3.cs
+               5 File(s)          4,842 bytes
+               2 Dir(s)  68,850,618,368 bytes free
---
+++ f4.cs
@@ -0,0 +1,15 @@
+ Volume in drive C is Windows8_OS
+ Volume Serial Number is B6DA-FC95
+
+ Directory of c:\git\demo2
+
+31/05/2015  15:21    <DIR>          .
+31/05/2015  15:21    <DIR>          ..
+19/12/2014  05:57               395 .gitattributes
+31/05/2015  15:17             3,511 .gitignore
+31/05/2015  15:17               425 f1.txt
+31/05/2015  15:18               511 f2.txt
+31/05/2015  15:20               512 f3.cs
+31/05/2015  15:21                 0 f4.cs
+               6 File(s)          5,354 bytes
+               2 Dir(s)  68,850,622,464 bytes free
